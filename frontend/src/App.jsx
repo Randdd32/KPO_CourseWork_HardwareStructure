c40d66754029a0f3ca1526f6f69e9c1c91f15fef
Илья <@WIN-RANNDDD>
2025-11-18 23:27:37 +0400

    Start working - add header and footer - change App.jsx - change index.css - delete unnecessary files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import PropTypes from 'prop-types';
+import { Container } from 'react-bootstrap';
+import { Toaster } from 'react-hot-toast';
+import { Outlet } from 'react-router-dom';
+import { SearchProvider } from './components/navigation/SearchContext.jsx';
+import Footer from './components/footer/Footer.jsx';
+import Navigation from './components/navigation/Navigation.jsx';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = ({ routes }) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <SearchProvider>
+      <Navigation routes={routes}></Navigation>
+      <Container className="d-flex flex-column flex-grow-1 my-1" as="main" fluid="md">
+        <Outlet />
+      </Container>
+      <Footer />
+      <Toaster position='top-center'
+        reverseOrder={true}
+        toastOptions={{
+          duration: 4000,
+        }} />
+    </SearchProvider>
+  );
+};
 
-export default App
+App.propTypes = {
+  routes: PropTypes.array,
+};
+
+export default App;
