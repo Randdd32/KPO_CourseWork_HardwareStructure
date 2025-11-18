5ab68c284abd8724001f08dadf0919d69e95ed2d
Илья <@WIN-RANNDDD>
2025-11-18 10:58:56 +0400

    Add DB population

--- backend/src/main/java/com/hardware/hardware_structure/HardwareStructureApplication.java
+++ backend/src/main/java/com/hardware/hardware_structure/HardwareStructureApplication.java
@@ -1,13 +1,28 @@
 package com.hardware.hardware_structure;
 
+import com.hardware.hardware_structure.core.setup.EntityInitializer;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
 
 @SpringBootApplication
-public class HardwareStructureApplication {
+@RequiredArgsConstructor
+public class HardwareStructureApplication implements CommandLineRunner  {
+	private final EntityInitializer initializer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HardwareStructureApplication.class, args);
 	}
 
+	@Override
+	@Transactional
+	public void run(String... args) {
+		if (args.length > 0 && Arrays.asList(args).contains("--populate")) {
+			initializer.initializeAll();
+		}
+	}
 }
