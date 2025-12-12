e4421a6794d6d66004daf4ef2338bfd04a1c1742
Илья <@WIN-RANNDDD>
2025-12-12 16:33:07 +0400

    Add dtos and constants for authentication

--- backend/src/main/java/com/hardware/hardware_structure/core/configuration/Constants.java
+++ backend/src/main/java/com/hardware/hardware_structure/core/configuration/Constants.java
@@ -9,9 +9,19 @@ public class Constants {
 
     public static final String ADMIN_PREFIX = "/admin";
 
+    public static final String LOGIN_URL = "/login";
+
+    public  static final String AUTH_URL = "/auth";
+
     public static final String DEFAULT_PAGE_SIZE = "16";
 
+    public static final Integer OTP_EXPIRATION_MINUTES = 10;
+
+    public static final String OTP_EMAIL_SUBJECT = "Двухфакторная аутентификация: запрос на вход в Ваш аккаунт";
+
     public static final int REFRESH_TOKEN_LIMIT = 5;
+
+    public static final String PASSWORD_PATTERN = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+\\\\-]).{8,60}$";
 
     public static final String NAME_PATTERN = "^[A-Za-zА-Яа-яЁё\\-]{1,50}$";
 }
