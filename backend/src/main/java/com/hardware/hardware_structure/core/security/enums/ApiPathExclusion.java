e65497a3c845d5c9ddd2a1022501435e2e0d3b48
Илья <@WIN-RANNDDD>
2025-12-17 11:51:24 +0400

    Add actuator path in ApiPathExclusion

--- backend/src/main/java/com/hardware/hardware_structure/core/security/enums/ApiPathExclusion.java
+++ backend/src/main/java/com/hardware/hardware_structure/core/security/enums/ApiPathExclusion.java
@@ -16,6 +16,7 @@ public enum ApiPathExclusion {
     REFRESH_TOKEN(Constants.API_URL + Constants.AUTH_URL + "/refresh-token"),
     LOGOUT(Constants.API_URL + Constants.AUTH_URL + "/logout"),
     LOGOUT_ALL(Constants.API_URL + Constants.AUTH_URL + "/logout-all"),
+    ACTUATOR("/actuator/**"),
     SWAGGER_UI("/swagger-ui/**"),
     SWAGGER_UI_HTML("/swagger-ui.html"),
     API_DOCS("/v3/api-docs/**");
